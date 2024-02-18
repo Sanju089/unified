11810b4a2300d6b1e9a2b88f731e6a0da0ff05d8
Sanjeev <arsanjeev.gupta@gmail.com>
2024-02-18 18:05:58 +0530

    herosectionchange1

--- src/Component/Slider/Slider1.jsx
+++ src/Component/Slider/Slider1.jsx
@@ -19,19 +19,29 @@ export default function Slider1() {
 
       </div>
       <div>
-        <h3>2</h3>
+        
+         <img src={slideImage} alt="slideImage" style={{width:"100%"}}/>
+
       </div>
       <div>
-        <h3>3</h3>
+       
+         <img src={slideImage} alt="slideImage" style={{width:"100%"}}/>
+
       </div>
       <div>
-        <h3>4</h3>
+       
+         <img src={slideImage} alt="slideImage" style={{width:"100%"}}/>
+
       </div>
       <div>
         <h3>5</h3>
+         <img src={slideImage} alt="slideImage" style={{width:"100%"}}/>
+
       </div>
       <div>
-        <h3>6</h3>
+        
+         <img src={slideImage} alt="slideImage" style={{width:"100%"}}/>
+
       </div>
     </Slider>
   );
